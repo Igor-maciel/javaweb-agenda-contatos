e2849e0930fa1f440132d5dc57ab49860de594a3
Igor-maciel <72346161+Igor-maciel@users.noreply.github.com>
2021-05-01 15:53:18 -0300

    metodos de acesso ao mysql

--- src/main/java/br/com/agenda/infra/ConexaoJdbc.java
+++ src/main/java/br/com/agenda/infra/ConexaoJdbc.java
@@ -1,5 +1,15 @@
 package br.com.agenda.infra;
 
-public interface ConexaoJdbc {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+
+public interface ConexaoJdbc {
+	Connection getConnection();
+
+    void close();
+
+    void commit() throws SQLException;
+
+    void rollback();
 }
--- src/main/java/br/com/agenda/infra/ConexaoMysql.java
+++ src/main/java/br/com/agenda/infra/ConexaoMysql.java
@@ -1,40 +1,65 @@
 package br.com.agenda.infra;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.sun.jdi.connect.spi.Connection;
 
-public class ConexaoMysql {
-	 private String driver = "com.mysql.cj.jdbc.Driver";
-	    // caminho onde o banco esta fisicamente
-	    private static final String DATABASE_URL =
-	            "jdbc:mysql://127.0.0.1:3306/agenda?useTimezone=true&serverTimezone=UTC";
+public class ConexaoMysql implements ConexaoJdbc{
+	
+	private Connection connection = null;
 
-	    // usuario do banco
-	    private static final String USERNAME = "root";
-	    // senha do banco
-	    private static final String PASSWORD ="";
+    //Driver de conexao om o banco
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    // caminho onde o banco esta fisicamente
+    private static final String DATABASE_URL =
+            "jdbc:mysql://172.17.0.2:3306/agenda?useTimezone=true&serverTimezone=UTC";
 
-	    private Connection conectar() {
-	        Connection connection = null;
-	        try {
-	            Class.forName(driver);
-	            connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-	            return connection;
-	        } catch (Exception e) {
-	            System.out.println(e);
-	            return null;
-	        }
-	    }
+    // usuario do banco
+    private static final String USERNAME = "root";
+    // senha do banco
+    private static final String PASSWORD = "";
 
-	    public void testaConexao() {
-	        try {
-	            Connection connection = conectar();
-	            System.out.println("Conectado com sucesso ->" + connection);
-	            connection.close();
-	        } catch (Exception e) {
-	            System.out.println(e);
-	        }
-	    }
+    public ConexaoMysql() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        this.connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        this.connection.setAutoCommit(false);
+    }
 
+    @Override
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    @Override
+    public void close() {
+        if (this.connection != null) {
+            try {
+                this.connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexaoMysql.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public void commit() throws SQLException {
+        this.connection.commit();
+        this.close();
+    }
+
+    @Override
+    public void rollback() {
+        if (this.connection != null) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexaoMysql.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                this.close();
+            }
+        }
+    }
 }
